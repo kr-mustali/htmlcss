40b72c306cc59e1187a9ec90c49b65e8a4f986d7
kr-mustali <mustali.c@knackroot.com>
2024-01-22 10:10:20 +0530

    js update

--- myscript.js
+++ myscript.js
@@ -1,3 +1,13 @@
 function myFunction() {
     document.getElementById("test").innerHTML = "Paragraph changed.";
   }
+function sumCal(){
+
+let x, y, z;  
+x = 4;        
+y = 5;        
+z = x + y;    
+
+document.getElementById("sum").innerHTML =z;  
+
+}  
