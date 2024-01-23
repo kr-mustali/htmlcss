096d69274bd20b6f9e0637c45fb89f322b04f95a
kr-mustali <mustali.c@knackroot.com>
2024-01-23 18:22:22 +0530

    promises

--- promise.js
+++ promise.js
@@ -1,40 +1,41 @@
 //promise
 
-let p = new Promise((resolve,reject)=>{
-    let x = 1+3;
+let p = new Promise((resolve, reject) => {
+  let x = 1 + 3;
 
-    if(x==2) resolve('Success')
-    else reject('Fail')
-})
+  if (x == 2) resolve("Success");
+  else reject("Fail");
+});
 
-p.then((msg)=>{
-    console.log('This is in then '+msg);
-}).catch((msg)=>{
-    console.log('THis is in the catch '+msg);
-})
+p.then((msg) => {
+  console.log("This is in then " + msg);
+}).catch((msg) => {
+  console.log("THis is in the catch " + msg);
+});
 
 //async/await
 
 async function myDisplay() {
-    let myPromise = new Promise(function(resolve, reject) {
-      resolve("Hello !!");
-    });
-    console.log(await myPromise);
-  }
-  
-  myDisplay();
+  let myPromise = new Promise(function (resolve, reject) {
+    resolve("Hello !!");
+  });
+  console.log(await myPromise);
+}
 
-  //promise.all
+myDisplay();
+
+//promise.all
 
 const promise1 = Promise.resolve(3);
 const promise2 = 42;
 const promise3 = new Promise((resolve, reject) => {
-//   setTimeout(resolve, 10000, 'foo');
-    let x=2;
-    if(x==2) resolve("It is Done");
-    else reject("Not Done");
+  //   setTimeout(resolve, 10000, 'foo');
+  let x = 2;
+  if (x == 2) resolve("It is Done");
+  else reject("Not Done");
 });
 
 Promise.all([promise1, promise2, promise3]).then((values) => {
   console.log(values);
 });
+const x = 10;
